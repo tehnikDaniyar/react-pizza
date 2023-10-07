af6604327a126c5c09b0925494229d58eb580938
Danik <tehnikdaniyar@gmail.com>
2023-10-07 10:30:45 +0600

    created react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Test from './components/test';
+function App() {
+  return (
+    <>
+    <Test />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+export default function Test() {
+
+return <>
+<div style={{width: "300px", height: "300px", backgroundColor: "green"}}></div>
+</>
+}
